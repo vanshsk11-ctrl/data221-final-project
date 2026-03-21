a18c1ba550e365e3010a68558faa8d9aa8081034
Jodha-06 <Jodhveerd06@gmail.com>
2026-03-20 19:55:46 -0600

    Add Random_Forest_Regression.py file to Git Repository for final project

---
+++ Random_Forest_Regression.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from sklearn.ensemble import RandomForestRegressor
+
